c7c95117506e1fd76e84b51edff912e53c5144aa
ngapps <ngapps.developer@gmail.com>
2025-04-13 20:21:00 +0200

    Update version

--- build-logic/convention/src/main/kotlin/config/Versioning.kt
+++ build-logic/convention/src/main/kotlin/config/Versioning.kt
@@ -23,7 +23,7 @@ internal object Versioning {
     const val ANDROID_TARGET_SDK = 35
     private const val VERSION_MAJOR = "1"
     private const val VERSION_MINOR = "0"
-    private const val VERSION_PATCH = "1"
+    private const val VERSION_PATCH = "2"
 
     val versionCode = VERSION_MAJOR.toInt() * 1000 + VERSION_MINOR.toInt() * 100 + VERSION_PATCH.toInt()
     val versionName = "$VERSION_MAJOR.$VERSION_MINOR.$VERSION_PATCH"
